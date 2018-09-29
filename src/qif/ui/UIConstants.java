7ee479dc4ab26880ac976f32529350ef6ab0e8a5
kcdietrich57 <gregdietrich57@gmail.com>
2018-09-29 16:03:11 -0500

    data update

---
+++ src/qif/ui/UIConstants.java
@@ -0,0 +1,10 @@
+package qif.ui;
+
+public class UIConstants {
+//	areaRenderer.setSeriesPaint(CREDIT, Color.YELLOW);
+//	areaRenderer.setSeriesPaint(LOAN, Color.MAGENTA);
+//	areaRenderer.setSeriesPaint(ASSET, Color.GREEN);
+//	areaRenderer.setSeriesPaint(RETIRE, Color.DARK_GRAY);
+//	areaRenderer.setSeriesPaint(BANK, Color.RED);
+//	areaRenderer.setSeriesPaint(INVEST, Color.BLUE);
+}
