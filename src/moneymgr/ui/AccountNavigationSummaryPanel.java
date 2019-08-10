020a0bc7eef98823722ffd9c5d920d1c644027bc
kcdietrich57 <gregdietrich57@gmail.com>
2019-08-10 07:45:31 -0500

    update

--- src/moneymgr/ui/AccountNavigationSummaryPanel.java
+++ src/moneymgr/ui/AccountNavigationSummaryPanel.java
@@ -11,6 +11,7 @@ import javax.swing.border.EmptyBorder;
 import moneymgr.report.NetWorthReporter;
 import moneymgr.report.NetWorthReporter.Balances;
 import moneymgr.util.Common;
+import moneymgr.util.QDate;
 
 /**
  * This panel displays a short summary of the overall status<br>
@@ -20,34 +21,50 @@ import moneymgr.util.Common;
  */
 @SuppressWarnings("serial")
 public class AccountNavigationSummaryPanel extends JPanel {
+	private JLabel nowTitle;
+	private JLabel asofTitle;
 	private JLabel assLabel;
 	private JLabel liabLabel;
 	private JLabel netLabel;
+	private JLabel assLabel2;
+	private JLabel liabLabel2;
+	private JLabel netLabel2;
 
 	public AccountNavigationSummaryPanel() {
 		super(new GridBagLayout());
 
 		setBorder(new EmptyBorder(10, 0, 10, 0));
 
-		Font bfont = new Font("Helvetica", Font.BOLD, 16);
-		Font font = new Font("Helvetica", Font.PLAIN, 16);
+		Font bfont = new Font("Helvetica", Font.BOLD, 14);
+		Font font = new Font("Helvetica", Font.PLAIN, 14);
 
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.fill = GridBagConstraints.BOTH;
 
-		this.assLabel = GridBagUtility.addLabeledValue(this, gbc, 0, 0, "Assets", bfont, font);
-		this.liabLabel = GridBagUtility.addLabeledValue(this, gbc, 1, 0, "Liabilities", bfont, font);
-		this.netLabel = GridBagUtility.addLabeledValue(this, gbc, 2, 0, "Net Worth", bfont, font);
+		this.nowTitle = GridBagUtility.addLabeledValue(this, gbc, 0, 0, "", font, bfont);
+		this.asofTitle = GridBagUtility.addValue(this, gbc, 0, 1, bfont);
+		this.assLabel = GridBagUtility.addLabeledValue(this, gbc, 1, 0, "Assets", font, font);
+		this.liabLabel = GridBagUtility.addLabeledValue(this, gbc, 2, 0, "Liabilities", font, font);
+		this.netLabel = GridBagUtility.addLabeledValue(this, gbc, 3, 0, "Net Worth", font, font);
+		this.assLabel2 = GridBagUtility.addValue(this, gbc, 1, 1, bfont);
+		this.liabLabel2 = GridBagUtility.addValue(this, gbc, 2, 1, bfont);
+		this.netLabel2 = GridBagUtility.addValue(this, gbc, 3, 1, bfont);
 
 		updateValues();
 	}
 
 	public void updateValues() {
-		Balances bals = NetWorthReporter.getBalancesForDate( //
+		Balances bals = NetWorthReporter.getBalancesForDate(QDate.today());
+		Balances bals2 = NetWorthReporter.getBalancesForDate( //
 				MainWindow.instance.asOfDate());
 
+		this.nowTitle.setText("Today");
+		this.asofTitle.setText("As Of");
 		this.assLabel.setText(Common.formatAmount(bals.assets));
 		this.liabLabel.setText(Common.formatAmount(bals.liabilities));
 		this.netLabel.setText(Common.formatAmount(bals.netWorth));
+		this.assLabel2.setText(Common.formatAmount(bals2.assets));
+		this.liabLabel2.setText(Common.formatAmount(bals2.liabilities));
+		this.netLabel2.setText(Common.formatAmount(bals2.netWorth));
 	}
 }
